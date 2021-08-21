69fec9467e05d1a7d909c41b7e3f8f35cd9558d7
Crowdbotics <team@crowdbotics.com>
2021-08-21 11:42:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "damp_fog_30007Identifier",
+  url: "https://damp_fog_30007Identifier.botics.co",
+  api: "https://damp_fog_30007Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
